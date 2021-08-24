d045fade6fd0533551c8f7ffd173152b9cabbb10
Adek <ajayimikeofficial@gmail.com>
2021-08-24 15:16:59 +0100

    add infura rpc url and handle wrong network error

--- client/src/connectors.js
+++ client/src/connectors.js
@@ -1,11 +1,17 @@
 import { InjectedConnector } from "@web3-react/injected-connector";
 import { WalletConnectConnector } from "@web3-react/walletconnect-connector";
+import { NetworkConnector } from "@web3-react/network-connector";
 
 
 const POLLING_INTERVAL = 8000;
 const RPC_URL = {
-    3: 'https://ropsten.infura.io/v3/507eafbfd2b647acbfd8a4cbb6f1b6f9'
+    4: 'https://rinkeby.infura.io/v3/507eafbfd2b647acbfd8a4cbb6f1b6f9'
 }
+
+export const network = new NetworkConnector({
+    urls: { 4: RPC_URL[4] },
+    defaultChainId: 4
+  })
 
 export const injected = new InjectedConnector({
     supportedChainIds: [4]
@@ -13,7 +19,7 @@ export const injected = new InjectedConnector({
 
 export const walletconnect = new WalletConnectConnector({
     rpc: {
-        3: RPC_URL[4],
+        4: RPC_URL[4],
         qrcode: true,
         pollingInterval: POLLING_INTERVAL
     }
